f9f3786069934b04d8ab26c3914c006a080be5b8
Venkateshwaran Selvaraj <venkateshwaran.selvaraj@email.com>
2022-02-09 17:07:35 +0530

    Added Toast component

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ function modalToggle(id){
   const modal = document.getElementById(id);
   modal.classList.toggle('show-modal');
 }
+
+function toastToggle(id) {
+  const toast =  document.querySelector(`#${id}`);
+  toast.classList.toggle('show-toast');
+
+  setTimeout(() => {
+    if(toast.classList.contains('show-toast')){
+      toast.classList.remove('show-toast');
+    }
+  } , 3000);
+}
